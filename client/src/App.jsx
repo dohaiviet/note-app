721d23f3be0e3604130ff0a516d23dd65765ebc0
vietdh <dohaiviet710@gmail.com>
2024-01-04 22:58:06 +0700

    init note app in server and client

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+
+function App() {
+
+  return <p>Note App</p>
+}
+
+export default App
---
+++ server/fake-data/index.js
@@ -0,0 +1,32 @@
+export default {
+    authors: [
+        {
+            id: 1,
+            name: 'vietdh'
+        },
+        {
+            id: 2,
+            name: 'vietdh2'
+        }
+    ],
+    folders: [
+        {
+            id: 1,
+            name: 'Home',
+            create_at: '01-01-2024',
+            author_id: 1
+        },
+        {
+            id: 2,
+            name: 'Work',
+            create_at: '01-01-2024',
+            author_id: 2
+        },
+        {
+            id: 3,
+            name: 'Note',
+            create_at: '01-01-2024',
+            author_id: 1
+        }
+    ]
+}
